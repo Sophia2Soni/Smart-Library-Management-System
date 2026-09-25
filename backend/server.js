6792e3b864bf0e2edef1e41535ea9e34aec37e33
Sophia Soni <sophiasoni49@gmail.com>
2026-09-26 00:28:47 +0530

    Configure port for deployment

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const csv = require('csv-parser');
 const db = require('./db');
 
 const app = express();
-const PORT = 5001;
+const PORT = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
